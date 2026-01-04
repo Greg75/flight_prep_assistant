bb9c4284ae31e37fe513980cb81f94487fe92130
Greg <100309099+Greg75@users.noreply.github.com>
2026-01-04 21:22:56 +0100

    test(input_data): add base test skeleton for InputData model

---
+++ flight_prep_assistant/tests/models/test_request_data.py
@@ -0,0 +1,15 @@
+class TestInputData:
+    def test_input_data_initializes_with_valid_data(self):
+        pass
+
+    def test_input_data_rejects_invalid_departure_airfield(self):
+        pass
+
+    def test_input_data_rejects_invalid_arrival_airfield(self):
+        pass
+
+    def test_input_data_rejects_invalid_aircraft_model(self):
+        pass
+
+    def test_input_data_missing_required_fields_raise_validation_error(self):
+        pass
